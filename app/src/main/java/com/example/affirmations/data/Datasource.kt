b02aac1897802a813269da8aaf37c21a8e41e235
luis <araujodasilvaluisgustavo@gmail.com>
2022-11-08 20:33:10 -0300

    MEME

---
+++ app/src/main/java/com/example/affirmations/data/Datasource.kt
@@ -0,0 +1,19 @@
+package com.example.affirmations.data
+import com.example.affirmations.R
+import com.example.affirmations.model.Affirmation
+class Datasource() {
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1, R.drawable._1),
+            Affirmation(R.string.affirmation2, R.drawable._2),
+            Affirmation(R.string.affirmation3, R.drawable._3),
+            Affirmation(R.string.affirmation4, R.drawable._4),
+            Affirmation(R.string.affirmation5, R.drawable._5),
+            Affirmation(R.string.affirmation6, R.drawable._6),
+            Affirmation(R.string.affirmation7, R.drawable._7),
+            Affirmation(R.string.affirmation8, R.drawable._8),
+            Affirmation(R.string.affirmation9, R.drawable._9),
+            Affirmation(R.string.affirmation10, R.drawable._10)
+        )
+    }
+}
